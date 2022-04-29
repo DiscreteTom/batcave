0b17f8047ace2225794a35d54b94a87cf8a55193
DiscreteTom <discrete_tom@outlook.com>
2022-04-29 09:03:41 +0800

    support default config entry

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import * as yaml from "js-yaml";
 import * as fs from "fs";
 import * as os from "os";
+import * as chalk from "chalk";
 
 type Config = {
   storage: {
@@ -8,7 +9,6 @@ type Config = {
     prefix: string;
     queueSize: number;
     partSize: number;
-    tagKey: string;
     profile: string;
     region: string;
   };
@@ -22,7 +22,21 @@ type Config = {
   useGitignore: boolean;
 };
 
+function must(condition: any, errMsg: string) {
+  if (!condition) console.log(chalk.red(errMsg));
+}
+
 let config = yaml.load(fs.readFileSync("config.yml", "utf-8")) as Config;
+must(config.storage.bucket, "Missing bucket name");
+config.storage.prefix ||= "";
+config.storage.queueSize ||= 4;
+config.storage.partSize ||= 5;
+config.storage.profile ||= "default";
+must(config.storage.region, "Missing bucket region");
+config.concurrent.limit ||= 100;
+config.concurrent.interval ||= 2000;
+
+// replace home dir in 'config.include'
 let home = os.homedir().replaceAll("\\", "/");
 config.include = config.include.map((i) =>
   i.startsWith("~") ? home + i.slice(1) : i
