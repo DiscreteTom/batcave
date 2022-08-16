53429af0268e75b30fc146f445fd1d56814f0cb9
DiscreteTom <discrete_tom@outlook.com>
2022-08-16 23:34:05 +0800

    chore: optimize code

--- src/lock.ts
+++ src/lock.ts
@@ -2,17 +2,16 @@ import config from "./config";
 
 let concurrent = 0;
 
-export default {
-  async lock(f: () => Promise<void>) {
-    while (concurrent >= config.concurrent.limit) {
-      // sleep
-      await new Promise((resolve) =>
-        setTimeout(resolve, config.concurrent.interval)
-      );
-    }
+async function sleep(ms: number) {
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
+}
 
-    concurrent++;
-    await f();
-    concurrent--;
-  },
-};
+/** Call f if the concurrent limit is not exceeded. */
+export async function lock(f: () => Promise<void>) {
+  while (concurrent >= config.transfer.concurrent.limit)
+    await sleep(config.transfer.concurrent.interval);
+
+  concurrent++;
+  await f();
+  concurrent--;
+}
