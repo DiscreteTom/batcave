7805c9d48739e21187f771076a5d805521c637cd
DiscreteTom <discrete_tom@outlook.com>
2022-08-18 22:35:04 +0800

    chore: use TaskPool to replace lock

--- src/lock.ts
+++ src/lock.ts
@@ -1,17 +1,45 @@
-import config from "./config";
-
-let concurrent = 0;
-
+/** Return a promise which will be resolved after the specified `ms`. */
 async function sleep(ms: number) {
   return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
 
-/** Call f if the concurrent limit is not exceeded. */
-export async function lock(f: () => Promise<void>) {
-  while (concurrent >= config.transfer.concurrent.limit)
-    await sleep(config.transfer.concurrent.interval);
+type Task = () => Promise<void>;
 
-  concurrent++;
-  await f();
-  concurrent--;
+/** Run async tasks with a concurrent limit. */
+export class TaskPool {
+  /** Concurrent limit. */
+  private limit: number;
+  /** Current concurrent. */
+  private concurrent: number;
+  /** How long to wait if the concurrent limit is reached, in ms. */
+  private interval: number;
+  /** Pool of promises. */
+  private pool: Promise<void>[];
+
+  constructor(limit: number, interval = 1000) {
+    this.concurrent = 0;
+    this.limit = limit;
+    this.interval = interval;
+    this.pool = [];
+  }
+
+  /** Execute the task if the concurrent under the limit. */
+  private async run(task: Task) {
+    // sleep until the concurrent under the limit
+    while (this.concurrent >= this.limit) await sleep(this.interval);
+
+    this.concurrent++;
+    await task();
+    this.concurrent--;
+  }
+
+  /** Submit a task to the pool, which will be executed if the concurrent under the limit. */
+  submit(task: Task) {
+    this.pool.push(this.run(task));
+  }
+
+  /** Return a promise which will be resolved after all tasks are done. */
+  async await() {
+    return Promise.all(this.pool);
+  }
 }
