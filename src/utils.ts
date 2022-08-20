57d3b13ae0d1b490aaf0ad6c9d8d3b60dc738b63
DiscreteTom <discrete_tom@outlook.com>
2022-08-20 22:50:19 +0800

    chore: remove unused utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,4 @@
-import * as chalk from "chalk";
-import { PathMapping } from "./model";
-
 /** Show error message if condition is false. */
 export function must(condition: any, errMsg: string) {
-  if (!condition) console.log(chalk.red(errMsg));
+  if (!condition) throw errMsg;
 }
