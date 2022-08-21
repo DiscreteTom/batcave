92317b776fbad5a1f2fc4d02ac1bfa8de5db6c82
DiscreteTom <discrete_tom@outlook.com>
2022-08-21 10:34:15 +0800

    feat: better config - check duplication for pm.remote - replace \ to / for pm.local - pm.remote can be omitted

--- src/config.ts
+++ src/config.ts
@@ -16,19 +16,35 @@ config.upload = customerConfig.upload || [];
 
 must(config.storage.bucket, `Bucket name can't be empty.`);
 
-// replace local home dir to absolute paths
-let home = homedir().replaceAll("\\", "/");
-function replaceHomeDir(pm: PathMapping) {
-  if (pm.local.startsWith("~")) pm.local = home + pm.local.slice(1);
+const home = homedir().replaceAll("\\", "/");
+function processLocal(pms: PathMapping[]) {
+  pms.map((pm) => {
+    // replace local home dir to absolute paths
+    if (pm.local.startsWith("~")) pm.local = home + pm.local.slice(1);
+    // replace `\` to `/` for windows system
+    pm.local = pm.local.replaceAll("\\", "/");
+  });
 }
-config.upload.map(replaceHomeDir);
-config.download.map(replaceHomeDir);
+processLocal(config.upload);
+processLocal(config.download);
 
-// replace remote path to S3 URI
-function replaceS3URI(pm: PathMapping) {
-  pm.remote = `s3://${config.storage.bucket}/${config.storage.prefix}${pm.remote}`;
+function processRemote(pms: PathMapping[]) {
+  const remoteSet = new Set<string>();
+
+  pms.map((pm) => {
+    // if no remote, defaults to the local folder name
+    if (!pm.remote) pm.remote = pm.local.split("/").at(-1);
+
+    // check duplication
+    if (remoteSet.has(pm.remote))
+      throw new Error(`Duplicated remote path: ${pm.remote}`);
+    remoteSet.add(pm.remote);
+
+    // replace remote path to S3 URI
+    pm.remote = `s3://${config.storage.bucket}/${config.storage.prefix}${pm.remote}`;
+  });
 }
-config.upload.map(replaceS3URI);
-config.download.map(replaceS3URI);
+processRemote(config.upload);
+processRemote(config.download);
 
 export default config;
